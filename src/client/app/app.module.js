7bf4fae19e2b207825c2aaf1d8ad9aee8a0113e7
John Papa <john@johnpapa.net>
2015-04-19 20:59:29 -0400

    made app module TS

---
+++ src/client/app/app.module.js
@@ -0,0 +1,13 @@
+var app;
+(function (app) {
+    'use strict';
+    angular.module('app', [
+        'app.core',
+        'app.widgets',
+        'app.admin',
+        'app.dashboard',
+        'app.layout'
+    ]);
+})(app || (app = {}));
+
+//# sourceMappingURL=../../client/app/app.module.js.map

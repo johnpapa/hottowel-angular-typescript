7c3b036cd5e234bafa6b7f73011a3c50b5006fcc
John Papa <john@johnpapa.net>
2015-04-26 15:35:13 -0400

    Revert "Revert "made debugging work. still more to do to enable node inspector again""
    This reverts commit b10919f45b710f76824c22a1d9020608d5a6f016.

--- gulpfile.js
+++ gulpfile.js
@@ -517,14 +517,15 @@ function orderSrc (src, order) {
  * @param  {Boolean} specRunner - server spec runner html
  */
 function serve(isDev, specRunner) {
-    var debug = args.debug || args.debugBrk;
-    var debugMode = args.debug ? '--debug' : args.debugBrk ? '--debug-brk' : '';
+//    var debug = args.debug || args.debugBrk;
+//    var debugMode = args.debug ? '--debug' : args.debugBrk ? '--debug-brk' : '';
+    var debugMode = '--debug';
     var nodeOptions = getNodeOptions(isDev);
 
-    if (debug) {
-        runNodeInspector();
+//    if (debug) {
+//        runNodeInspector();
         nodeOptions.nodeArgs = [debugMode + '=5858'];
-    }
+//    }
 
     if (args.verbose) {
         console.log(nodeOptions);

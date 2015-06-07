ef90457101d994d3812a43606b537a2e60c07fb3
John Papa <john@johnpapa.net>
2015-06-07 10:29:54 -0400

    comments about the hack

--- src/client/app/app.module.js
+++ src/client/app/app.module.js
@@ -6,5 +6,14 @@ angular.module('app', [
     'app.dashboard',
     'app.layout'
 ]);
+/**
+ * TODO: Exports is here because we are exporting typescript
+ *  components. Why? Because the tsc likes to know where the
+ *  types I am referencing are coming from. We compile to
+ *  commonjs, which means exports. We can use browserify
+ *  later, but for a hack job we can just declare exports, as
+ *  these are not actually used in the code.
+ * Definately want to clean this.
+ */
 var exports = {};
 //# sourceMappingURL=app.module.js.map

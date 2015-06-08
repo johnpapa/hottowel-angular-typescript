855f48b1edfbdd5c33a64a5d1ed6c87d00c31b02
John Papa <john@johnpapa.net>
2015-06-07 20:45:04 -0400

    oops

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -8,16 +8,16 @@ angular.module('app', [
     'app.layout'
 ]);
 /**
-Exports WAS here because we are exporting typescript
-components. Why? Because the tsc likes to know where the
-types I am referencing are coming from. We compile to
-commonjs, which means exports. We can use browserify
-later, but for a hack job we can just declare exports, as
-these are not actually used in the code.
-Definately want to clean this.
-But wait ... exports is commented out ... this is because 
-for now we have only exported interfaces. This satisfies 
-the tooling and since we are not using module loading, 
-we're OK.
+ * Exports WAS here because we are exporting typescript
+ * components. Why? Because the tsc likes to know where the
+ * types I am referencing are coming from. We compile to
+ * commonjs, which means exports. We can use browserify
+ * later, but for a hack job we can just declare exports, as
+ * these are not actually used in the code.
+ * Definately want to clean this.
+ * But wait ... exports is commented out ... this is because 
+ * for now we have only exported interfaces. This satisfies 
+ * the tooling and since we are not using module loading, 
+ * we're OK.
  */
 // var exports = {};

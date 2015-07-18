efc6b5e9c31bc060cea4099849c7b9e1c3f20559
John Papa <john@johnpapa.net>
2015-07-18 12:52:42 -0400

    config for system.js

---
+++ src/client/config.js
@@ -0,0 +1,18 @@
+System.config({
+    // transpiler: 'typescript',
+    // typescriptOptions: {
+    //     emitDecoratorMetadata: true,
+    //     experimentalDecorators: true
+    // }
+    // defaultJSExtensions: false, //use defaultJSExtensions = false , this is a bug in es6-module-loader 0.17 and should be fixed in next release
+
+    paths: {
+        "rx": "rx"
+    }
+});
+// System.paths = {
+//     "main": "main.ts",
+//     "tooltip": "tooltip.ts",
+//     "mycomponent": "mycomponent.ts",
+//     "github.service": "github.service.ts"
+// };
